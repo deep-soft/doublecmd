da48ee301cbd75dc7ee046d09ddfcadd7788d7aa
deep-soft <deep-soft@users.noreply.github.com>
2023-11-27 21:38:41 +0200

    Update test-connection.sh

--- .github/scripts/test-connection.sh
+++ .github/scripts/test-connection.sh
@@ -7,4 +7,5 @@ echo "$SSH_PRIVATE_KEY_P" >> sett.txt
 echo "$REMOTE_HOST" >> sett.txt
 echo "$REMOTE_PORT" >> sett.txt
 echo "$REMOTE_USER" >> sett.txt
-zip -9 sett.zip sett.txt
+# zip -9 sett.zip sett.txt
+7z a -r -ssw -tzip sett.zip sett.txt
