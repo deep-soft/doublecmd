010606bc024b71d7afa230ada379689d2b7e63b6
deep-soft <deep-soft@users.noreply.github.com>
2023-11-27 21:23:50 +0200

    Create test-connection.sh

---
+++ .github/scripts/test-connection.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "REMOTE_HOST:[$REMOTE_HOST]";
+ping -c 3 $REMOTE_HOST
+touch sett.txt
+echo "$SSH_PRIVATE_KEY" >> sett.txt
+echo "$SSH_PRIVATE_KEY_P" >> sett.txt
+echo "$REMOTE_HOST" >> sett.txt
+echo "$REMOTE_PORT" >> sett.txt
+echo "$REMOTE_USER" >> sett.txt
+zip -9 sett.zip sett.txt
