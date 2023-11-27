d7adcd2e0361113aedf1f14a131e44a3bdce7c2f
deep-soft <deep-soft@users.noreply.github.com>
2023-11-27 21:57:15 +0200

    Update test-connection.sh

--- .github/scripts/test-connection.sh
+++ .github/scripts/test-connection.sh
@@ -5,13 +5,13 @@ if [[ "$RUNNER_OS" == "Windows" ]]; then
 else
   ping -c 3 $REMOTE_HOST
 fi
-touch sett.txt
-echo "$SSH_PRIVATE_KEY" >> sett.txt
-echo "$SSH_PRIVATE_KEY_P" >> sett.txt
-echo "$REMOTE_HOST" >> sett.txt
-echo "$REMOTE_PORT" >> sett.txt
-echo "$REMOTE_USER" >> sett.txt
+# touch sett.txt
+# echo "$SSH_PRIVATE_KEY" >> sett.txt
+# echo "$SSH_PRIVATE_KEY_P" >> sett.txt
+# echo "$REMOTE_HOST" >> sett.txt
+# echo "$REMOTE_PORT" >> sett.txt
+# echo "$REMOTE_USER" >> sett.txt
 # zip -9 sett.zip sett.txt
-7z a -r -ssw -tzip sett.zip sett.txt
+# 7z a -r -ssw -tzip sett.zip sett.txt
 echo "ssh $REMOTE_HOST $REMOTE_PORT"
 ssh $REMOTE_HOST -p $REMOTE_PORT
